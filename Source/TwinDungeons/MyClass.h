939c17733236a6bba59ed2a50eb66aa4bd715df3
Guillaume.D <76114741+DavidGuillaumeDev@users.noreply.github.com>
2024-02-13 00:34:38 +0100

    Add MultiPlayer

---
+++ Source/TwinDungeons/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TWINDUNGEONS_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
